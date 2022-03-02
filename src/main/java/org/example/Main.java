407e8f5ceedb7c92e5701713a7095cfd3445e689
kriti <kriti.dhingra@credable.in>
2022-03-02 10:28:31 +0530

    .gitignore added

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+import java.sql.Driver;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        FirefoxOptions options = new FirefoxOptions();
+        options.setProfile(new FirefoxProfile());
+        options.setBinary("/usr/bin/firefox");
+
+        WebDriver driver = new FirefoxDriver(options);
+
+        driver.get("http://google.com");
+
+        // document.getElementsByName("q")[0]
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.click();
+
+        // document.getElementsByName("q")[0].value = "Selenium learning"
+        searchBox.sendKeys("Selenium learning");
+        searchBox.sendKeys(Keys.ENTER);
+    }
+
+}
