f62ef0f77fd3dcedbcb12e18fcb9811d4eaccdcf
kriti <kriti.dhingra@credable.in>
2022-03-04 14:17:20 +0530

    test page for checkbox and print message

---
+++ src/test/java/org/example/CheckBoxTest.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class CheckBoxTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setup()
+    {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        driver.get("http://localhost:8082");
+        WebElement id = driver.findElement(By.id("checkboxLink"));
+        id.click();
+    }
+
+    @Test
+
+    public void  test_checkbox() {
+
+//        WebDriver driver = new FirefoxDriver();
+
+//        driver.get("http://localhost:8082");
+
+//        WebElement id = driver.findElement(By.id("checkboxLink"));
+//        id.click();
+
+        WebElement Lattuce = driver.findElement(By.id("lettuceCheckbox"));
+        Lattuce.click();
+
+        assertTrue(Lattuce.isSelected());
+    }
+
+    @Test
+    public void test_message()
+    {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("message")));
+
+        WebElement msg = driver.findElement(By.id("message"));
+        assertEquals("Loading complete...", msg.getText());
+    }
+
+    @After
+    public void close()
+    {
+        driver.quit();
+    }
+
+}
--- src/test/java/org/example/SeleniumWebsiteTest.java
+++ src/test/java/org/example/SeleniumWebsiteTest.java
@@ -1,6 +1,10 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,20 +13,38 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import static jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle.title;
 import static org.junit.Assert.assertEquals;
 
-public class Test {
+public class SeleniumWebsiteTest {
+
+    private WebDriver driver;
 
     @org.junit.Test
 
-    public void  test_selenium_dot_dev_blog()
+//    @BeforeClass
+//    public static void init()
+//    {
+////        WebDriverManager.firefoxdriver().setup();
+//    }
+
+    @Before
+    public void setup()
     {
         WebDriverManager.firefoxdriver().setup();
+         driver = new FirefoxDriver();
+         driver.get("http://selenium.dev");
+         WebElement blog = driver.findElement(By.linkText("Blog"));
+         blog.click();
+    }
 
-        WebDriver driver = new FirefoxDriver();
-
-        driver.get("https://selenium.dev");
-
-        WebElement blog = driver.findElement(By.linkText("Blog"));
-        blog.click();
+    public void  test_selenium_dot_dev_blog()
+    {
+//        WebDriverManager.firefoxdriver().setup();
+//
+//        WebDriver driver = new FirefoxDriver();
+//
+//        driver.get("https://selenium.dev");
+//
+//        WebElement blog = driver.findElement(By.linkText("Blog"));
+//        blog.click();
 
         WebElement link2016 = driver.findElement(By.id("m-blog2016"));
         link2016.click();
@@ -30,13 +52,13 @@ public class Test {
         WebElement element = driver.findElement(By.linkText("Fall Selenium Conf, Save the Date & Call for Speakers!!"));
         assertEquals("Blog Posts - 2016 | Selenium", title);
 
-        driver.quit();
+        //driver.quit();
     }
 
     @org.junit.Test
     public void  test_selenium_dot_dev_blog_2017_page()
     {
-        WebDriverManager.firefoxdriver().setup();
+//        WebDriverManager.firefoxdriver().setup();
 
         WebDriver driver = new FirefoxDriver();
 
@@ -51,6 +73,19 @@ public class Test {
         WebElement element = driver.findElement(By.linkText("Fall Selenium Conf, Save the Date & Call for Speakers!!"));
         assertEquals("Blog Posts - 2017 | Selenium", title);
 
+        //driver.quit();
+    }
+
+    @After
+    public void close()
+    {
+        System.out.println("close after class");
         driver.quit();
+    }
+
+    @AfterClass
+    public static void afterclass()
+    {
+        System.out.println("inside after class");
     }
 }
